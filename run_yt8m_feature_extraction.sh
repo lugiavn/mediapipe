2443cd819bfe4fa85222cf5207cb94b2621b594d
Nam Vo <nam.poke@gmail.com>
2022-01-16 17:23:43 -0800

    Create run_yt8m_feature_extraction.sh

---
+++ run_yt8m_feature_extraction.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Example usage (absolute path)
+#  ./run_yt8m_feature_extraction.sh /root/test.mp4 /tmp/
+INPUT_FILE=$1
+OUTPUT_DIR=$2
+
+MEDIAPIPE_DIR="/home/ec2-user/mediapipe"
+DOCKER_MEDIAPIPE_DIR="/mediapipe"
+
+#############################################
+# Outside docker call
+#############################################
+docker ps
+retVal=$?
+if [ $retVal -eq 0 ]; then
+  echo "======================================================="
+  echo "Calling docker exec"
+  echo "======================================================="
+  docker cp ${MEDIAPIPE_DIR}/run_yt8m_feature_extraction.sh mediapipe:${DOCKER_MEDIAPIPE_DIR}
+  docker cp $INPUT_FILE mediapipe:/root/input.mp4
+  docker exec mediapipe ${DOCKER_MEDIAPIPE_DIR}/run_yt8m_feature_extraction.sh /root/input.mp4 /root/yt8m_feature_output
+  docker cp mediapipe:/root/yt8m_feature_output ${OUTPUT_DIR}
+  exit 0
+fi
+
+
+############################################
+# Command run inside docker
+############################################
+echo "===================================================="
+echo "Run mediapipe in docker"
+echo "===================================================="
+
+curDir=$(pwd)
+cd $DOCKER_MEDIAPIPE_DIR
+
+python -m mediapipe.examples.desktop.youtube8m.generate_input_sequence_example \
+  --path_to_input_video=${INPUT_FILE} \
+  --clip_end_time_sec=21
+
+GLOG_logtostderr=1 /mediapipe/bazel-bin/mediapipe/examples/desktop/youtube8m/extract_yt8m_features \
+  --calculator_graph_config_file=mediapipe/graphs/youtube8m/feature_extraction.pbtxt \
+  --input_side_packets=input_sequence_example=/tmp/mediapipe/metadata.pb  \
+  --output_side_packets=output_sequence_example=/tmp/mediapipe/features.pb
+
+cd $curDIR
+
+mkdir -p ${OUTPUT_DIR}
+cp /tmp/mediapipe/metadata.pb ${OUTPUT_DIR}
+cp /tmp/mediapipe/features.pb ${OUTPUT_DIR}
+
